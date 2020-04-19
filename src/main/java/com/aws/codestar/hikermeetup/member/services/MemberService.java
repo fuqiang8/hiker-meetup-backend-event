8b2d573711932ea0ea312a343bc17d8a1ecbaca3
Fu Qiang Wong <fuqiang20@gmail.com>
2020-04-19 21:30:05 +0800

    Removed redundant methods which were created for testing from Member

--- src/main/java/com/aws/codestar/hikermeetup/member/services/MemberService.java
+++ src/main/java/com/aws/codestar/hikermeetup/member/services/MemberService.java
@@ -1,14 +1,8 @@
 package com.aws.codestar.hikermeetup.member.services;
 
-import com.aws.codestar.hikermeetup.member.data.Member;
 import com.aws.codestar.hikermeetup.member.data.MemberRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityNotFoundException;
-import java.util.UUID;
 
 @Service
 @Transactional
@@ -18,26 +12,5 @@ public class MemberService {
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
-    @Transactional(readOnly = true)
-    public Page<Member> getMembers(Pageable pageable) {
-        return memberRepository.findAll(pageable);
-    }
-
-    @Transactional(readOnly = true)
-    public Member getMember(UUID memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new EntityNotFoundException("Could not find member " + memberId));
-    }
-
-    public Member createMember(UUID userId, String name) {
-        Member member = new Member(userId, name);
-
-        return memberRepository.save(member);
-    }
-
-    public Member getOrCreateMember(UUID userId, String name) {
-        return memberRepository.findById(userId)
-                .orElse(createMember(userId,name));
-    }
+    
 }
