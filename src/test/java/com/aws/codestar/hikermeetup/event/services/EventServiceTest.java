754846522c6204435f258a18eb81d0cce64dde59
Fu Qiang Wong <fuqiang20@gmail.com>
2020-04-23 10:57:44 +0800

    Added tests for update events slong with a helper methods.

--- src/test/java/com/aws/codestar/hikermeetup/event/services/EventServiceTest.java
+++ src/test/java/com/aws/codestar/hikermeetup/event/services/EventServiceTest.java
@@ -4,7 +4,9 @@ import com.aws.codestar.hikermeetup.base.PatchModelMapper;
 import com.aws.codestar.hikermeetup.event.data.Event;
 import com.aws.codestar.hikermeetup.event.data.EventRepository;
 import com.aws.codestar.hikermeetup.event.data.EventStatus;
+import com.aws.codestar.hikermeetup.event.exceptions.EventStatusException;
 import com.aws.codestar.hikermeetup.event.web.EventInput;
+import com.aws.codestar.hikermeetup.member.data.Member;
 import com.aws.codestar.hikermeetup.member.services.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,10 +15,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 
 class EventServiceTest {
@@ -58,7 +64,84 @@ class EventServiceTest {
     }
 
     @Test
-    void updateEvent() {
+    void updateEvent_DecreaseMinAttendeeToGreenLit() {
+        Member organizer = generateMember("organizer");
+        when(memberService.getOrCreateCurrentMember()).thenReturn(organizer);
+        setupSuccessfulEventRetrieval(organizer, EventStatus.PENDING, 5);
+        setupMinAttendeeMapper();
+
+        Integer newMin = Integer.valueOf(2);
+        EventInput eventInput = new EventInput();
+        eventInput.setMinAttendees(newMin);
+
+        Event result = eventService.updateEvent(UUID.randomUUID(), eventInput);
+        assertEquals(EventStatus.GREENLIT, result.getEventStatus());
+        assertEquals(newMin, result.getMinAttendees());
+    }
+
+    @Test()
+    void updateEvent_IncreaseMinAttendeeToPending() {
+        Member organizer = generateMember("organizer");
+        when(memberService.getOrCreateCurrentMember()).thenReturn(organizer);
+        setupSuccessfulEventRetrieval(organizer, EventStatus.GREENLIT, 5);
+        setupMinAttendeeMapper();
+
+        Integer newMin = Integer.valueOf(7);
+        EventInput eventInput = new EventInput();
+        eventInput.setMinAttendees(newMin);
+
+        Event result = eventService.updateEvent(UUID.randomUUID(), eventInput);
+        assertEquals(EventStatus.PENDING, result.getEventStatus());
+        assertEquals(newMin, result.getMinAttendees());
+    }
+
+    @Test
+    void updateEvent_MatchMinAttendeeToGreenLit() {
+        Member organizer = generateMember("organizer");
+        when(memberService.getOrCreateCurrentMember()).thenReturn(organizer);
+        setupSuccessfulEventRetrieval(organizer, EventStatus.PENDING, 5);
+        setupMinAttendeeMapper();
+
+        Integer newMin = Integer.valueOf(5);
+        EventInput eventInput = new EventInput();
+        eventInput.setMinAttendees(newMin);
+
+        Event result = eventService.updateEvent(UUID.randomUUID(), eventInput);
+        assertEquals(EventStatus.GREENLIT, result.getEventStatus());
+        assertEquals(newMin, result.getMinAttendees());
+    }
+
+    @Test
+    void updateEvent_EventStarted() {
+        setupSuccessfulEventRetrieval(generateMember("organizer"), EventStatus.STARTED);
+
+        Exception exception = assertThrows(EventStatusException.class, () -> {
+            eventService.updateEvent(UUID.randomUUID(), new EventInput());
+        });
+
+        assertTrue(exception.getMessage().contains("had started"));
+    }
+
+    @Test
+    void updateEvent_EventFinished() {
+        setupSuccessfulEventRetrieval(generateMember("organizer"), EventStatus.FINISHED);
+
+        Exception exception = assertThrows(EventStatusException.class, () -> {
+            eventService.updateEvent(UUID.randomUUID(), new EventInput());
+        });
+
+        assertTrue(exception.getMessage().contains("had finished"));
+    }
+
+    @Test
+    void updateEvent_EventCanceled() {
+        setupSuccessfulEventRetrieval(generateMember("organizer"), EventStatus.CANCELED);
+
+        Exception exception = assertThrows(EventStatusException.class, () -> {
+            eventService.updateEvent(UUID.randomUUID(), new EventInput());
+        });
+
+        assertTrue(exception.getMessage().contains("had been canceled"));
     }
 
     @Test
@@ -83,5 +166,40 @@ class EventServiceTest {
 
     @Test
     void removeAttendee() {
+    }
+
+    private Member generateMember(String name) {
+        Member member = new Member(UUID.randomUUID(), name);
+        ReflectionTestUtils.setField(member, "id", UUID.randomUUID());
+        return member;
+    }
+
+    private void setupMinAttendeeMapper() {
+        doAnswer(i -> {
+            Integer min = (Integer) ReflectionTestUtils.getField((EventInput) i.getArgument(0), "minAttendees");
+
+            ReflectionTestUtils.setField((Event) i.getArgument(1), "minAttendees", min);
+            return i.getArgument(1);
+        }).when(mapper).map(any(EventInput.class), any(Event.class));
+    }
+
+    private void setupSuccessfulEventRetrieval(Member organizer, EventStatus currentEventStatus) {
+        setupSuccessfulEventRetrieval(organizer, currentEventStatus, 0);
+    }
+
+    private void setupSuccessfulEventRetrieval(Member organizer, EventStatus currentEventStatus, int currentAttendees) {
+        Set<Member> attendees = new HashSet<>();
+        for (int i = 0; i < currentAttendees; i++) {
+            attendees.add(generateMember("attendee" + i));
+        }
+
+        Event event = new Event(organizer);
+        event.setAttendees(attendees);
+        event.setEventStatus(currentEventStatus);
+
+        when(eventRepository.findById(any(UUID.class))).thenAnswer(i -> {
+            ReflectionTestUtils.setField(event, "id", i.getArgument(0));
+            return Optional.of(event);
+        });
     }
 }
