23a806d6e14a92c5561c24836bc60dfb0cac8768
Fu Qiang Wong <fuqiang20@gmail.com>
2020-05-04 13:21:53 +0800

    Removing keys from code. This is big security breach. AWS would complain about this and restrict access.

--- src/main/java/com/aws/codestar/hikermeetup/base/dynamodb/DynamoDBConfig.java
+++ src/main/java/com/aws/codestar/hikermeetup/base/dynamodb/DynamoDBConfig.java
@@ -1,10 +1,12 @@
 package com.aws.codestar.hikermeetup.base.dynamodb;
 
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.aws.codestar.hikermeetup.event.data.EventRepository;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,19 +20,15 @@ public class DynamoDBConfig {
     @Value("${dynamodb.endpoint}")
     private String endpoint;
 
-    @Value("${dynamodb.accesskey}")
-    private String accessKey;
-
-    @Value("${dynamodb.secretkey}")
-    private String secretKey;
+    @Value("${dynamodb.region}")
+    private String region;
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        AmazonDynamoDB amazonDynamoDB
-                = new AmazonDynamoDBClient(new BasicAWSCredentials(accessKey, secretKey));
-
-        amazonDynamoDB.setEndpoint(endpoint);
-        amazonDynamoDB.setRegion(Region.getRegion(Regions.AP_SOUTHEAST_1));
+        AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .build();
 
         return amazonDynamoDB;
     }
